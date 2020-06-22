b356fd621948ff47f82379b165498824e97f0afa
LenaKurasheva <lena.kurasheva@gmail.com>
2020-06-22 13:30:09 +0300

    homework2

---
+++ src/homework/lesson_2/multiscene/ChatLauncher.java
@@ -0,0 +1,8 @@
+package homework.lesson_2.multiscene;
+
+//запускаем гуи программу
+public class ChatLauncher {
+    public static void main(String[] args) {
+        ChatSceneApp.main(args);
+    }
+}
---
+++ src/homework/lesson_2/multiscene/Stageable.java
@@ -0,0 +1,7 @@
+package homework.lesson_2.multiscene;
+
+import javafx.stage.Stage;
+
+public interface Stageable {
+    void setStage(Stage stage);
+}
